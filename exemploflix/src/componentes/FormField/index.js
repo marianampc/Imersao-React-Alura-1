28b00d7d1639ac5fe950a32273e0bb297f01bafc
marianampc <Mariana13MPC@gmail.com>
2020-08-05 17:44:05 -0300

    1h do quarto dia

--- exemploflix/src/componentes/FormField/index.js
+++ exemploflix/src/componentes/FormField/index.js
@@ -1,22 +1,129 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
-function FormField({ label, type, name, value, onChange }) {
+//Colocamos os nomes trocado, no formato brasileiro com a escrita em inglês (meio confuso)
+const FormFieldWrapper = styled.div`
+  position: relative;
+  textarea {
+    min-height: 150px;
+  }
+  input[type="color"] {
+    padding-left: 56px;
+  }
+`;
+
+const Label = styled.label``;
+
+Label.Text = styled.span`
+  color: #E5E5E5;
+  height: 57px;
+  position: absolute; 
+  top: 0;
+  left: 16px;
+  
+  display: flex;
+  align-items: center;
+  
+  transform-origin: 0% 0%;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+  
+  transition: .1s ease-in-out;
+`;
+
+const Input = styled.input`
+  background: #53585D;
+  color: #F5F5F5;
+  display: block;
+  width: 100%;
+  height: 57px;
+  font-size: 18px;
+  
+  outline: 0;
+  border: 0;
+  border-top: 4px solid transparent;
+  border-bottom: 4px solid #53585D;
+  
+  padding: 16px 16px;
+  margin-bottom: 45px;
+  
+  resize: none;
+  border-radius: 4px;
+  transition: border-color .3s;
+  
+  &:focus {
+    border-bottom-color: var(--primary);
+  }
+  {/*Essa tag abaixo está focada no Label.Text dentro da caixa do input, lemrando que começamos com o span.
+    Também estamos programando o css à não aceitar mudanças quando for color*/}
+  &:focus:not([type='color']) + ${Label.Text} {
+    transform: scale(.6) translateY(-10px);
+  }
+  ${({ value }) => {
+    const hasValue = value.length > 0;
+    return hasValue && css`
+        &:not([type='color']) + ${Label.Text} {
+          transform: scale(.6) translateY(-10px); /*Essa é a animação do label mudar de tamanho*/
+        }
+      `;
+  }
+}
+`;
+/*const Input = styled.input`
+
+`;Isso permite que possamos colocar o Input, assim fazendo funcionar o as="textarea".
+  Depois foi substituido no início do doc.
+*/
+function FormField({
+  label, type, name, value, onChange,
+}) {
   const fieldID = `id_${name}`;
+  
+  const isTypeTextarea = type === 'textarea';
+  const tag = isTypeTextarea ? 'textarea' : 'input';
+  //Essa linha abaixo é a junção das duas acima.
+  //const tag = type === 'textarea' ? 'textarea' : 'input';//Iremos colocar isso para que o input seja textarea somente quando a entrada também for.
+  
+  const hasValue = Boolean(value.length);
+  
   return (
-    <div>
-      <label
+    <FormFieldWrapper>
+      <Label
         htmlFor={fieldID}
       >
-        {label}: 
-        <input
+        <Input
+          as={tag}
           type={type}
           value={value}
           name={name}
-          onChange={onChange} //O onChage dentro da função do onChange é um componente não inteligente. Pois nesse componente ele não sabe a função que está entrando.
+          hasValue={hasValue}
+          onChange={onChange}
         />
-      </label>
-    </div>
-  )
+        <Label.Text> {/*Essa teg foi colocada para ter uma facilidade na manutenção do estilo do texto, além de fazer aparecer a label dentro da caixa de escrita no início */}
+          {label}
+          :
+        </Label.Text>
+      </Label>
+    </FormFieldWrapper>
+  );
 }
+
+FormField.defaultProps = {
+  types: 'text',
+  valeu: '',
+  onChange: () => {},
+};
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  // eslint-disable-next-line max-len
+  type: PropTypes.string, // Como esse comando não é obrigatório, significa que devemos estabelecer um valor deafault para ele.
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
 
 export default FormField;
--- exemploflix/src/pages/Cadastro/Categoria/index.js
+++ exemploflix/src/pages/Cadastro/Categoria/index.js
@@ -1,116 +1,144 @@
-import React from 'react'
+/* eslint-disable no-lone-blocks */
+/* eslint-disable linebreak-style */
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../componentes/PageDefault';
 import FormField from '../../../componentes/FormField';
+import Button from '../../../componentes/Button';
 
-function CadastroCategoria(){
-    const valoresIniciais = {
-        nome: '',
-        descricao: '',
-        cor: ''
-    } 
-    const [categorias, setCategorias] = React.useState([]);
-    {/*Quando criamos três entradas para o formulário (nome, descrição e cor) uma solução abordada para diferenciar as informações seria criar três estados diferentes. Contudo no javascricript podemos criar um objeto com as três. 
-    */}
-    const [values, setValues] = React.useState(valoresIniciais); {/* Esse useState é nosso valor inicial*/}
+function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  };
+  const [categorias, setCategorias] = useState([]);
+  /* Quando criamos três entradas para o formulário (nome, descrição e cor) uma solução abordada para diferenciar as informações seria criar três estados diferentes. Contudo no javascricript podemos criar um objeto com as três.
+    */ 
+  // eslint-disable-next-line max-len
+  const [values, setValues] = useState(valoresIniciais); { /* Esse useState é nosso valor inicial */ }
 
-    //A chave pode ser o nome, descricao, cor, etc 
-    function setValeu(chave, valor){
-        setValues({
-            ...values,
-            [chave]: valor,
-            //O [] transforma a variável chave em um valor dinâmico
-        })
-    }
-    function handleChange(infosDoEvento){
-        setValeu(
-            infosDoEvento.target.getAttribute('name'), 
-            infosDoEvento.target.value
-        )
-    }
-    
-    return (
-        <PageDefault>
-            {/*nomeDaCategoria se tornou um variável*/}
-            <h1>Cadastro de Categoria: {values.nome}</h1>
-            {/*Com essa função o cadastro funcionou, só não sei onde está salvando a informação*/}
-            <form onSubmit={function handleSubmit(infosDoEvento){
-                infosDoEvento.preventDefault();
-                {/*Com essa linha, a página para de carregar quando se envia o formulário 
-                console.log('Você tentou enviar o form, né ?');*/}
-                
-                {/*Esse comando setCategorias, é uma função que recebeu uma lista com um item. Logo precisa retornar à mesma coisa*/}
-                setCategorias([
-                    ...categorias, 
-                    values.nome
-                ]);
-                {/*Esses três pontos está falando para pegar todas as categorias, salvar e não sobreescrever, e adicionar a informação abaixo*/}
-                setValues(valoresIniciais)
-            }}> 
-                <FormField
-                label="Nome da Categoria"
-                type="text"
-                name="nome"
-                value={values.nome}
-                onChange={handleChange}
-                />
-                {/*------CATEGORIA --------------*/}
-                {/*<div> 
+  // A chave pode ser o nome, descricao, cor, etc
+  function setValeu(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+      // O [] transforma a variável chave em um valor dinâmico
+    });
+  }
+  function handleChange(infosDoEvento) {
+    setValeu(
+      infosDoEvento.target.getAttribute('name'),
+      infosDoEvento.target.value,
+    );
+  }
+
+  //____________AULA 4________________________________ 
+  useEffect(() => {
+    if(window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias'; 
+      fetch(URL)
+       .then(async (respostaDoServer) =>{
+        if(respostaDoServer.ok) {
+          const resposta = await respostaDoServer.json();
+          setCategorias(resposta);
+          return; 
+        }
+        throw new Error('Não foi possível pegar os dados');
+       })
+    }    
+  }, []);
+  //__________________________________________________
+
+  return (
+    <PageDefault>
+      {/* nomeDaCategoria se tornou um variável */}
+      <h1>
+        Cadastro de Categoria:
+        {values.nome}
+      </h1>
+      {/* Com essa função o cadastro funcionou, só não sei onde está salvando a informação */}
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        { /* Com essa linha, a página para de carregar quando se envia o formulário
+                console.log('Você tentou enviar o form, né ?'); */ }
+
+        { /* Esse comando setCategorias, é uma função que recebeu uma lista com um item. Logo precisa retornar à mesma coisa */ }
+        setCategorias([
+          ...categorias,
+          values.nome,
+        ]);
+        { /* Esses três pontos está falando para pegar todas as categorias, salvar e não sobreescrever, e adicionar a informação abaixo */ }
+        setValues(valoresIniciais);
+      }}
+      >
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+        {/* ------CATEGORIA --------------*/}
+        {/* <div>
                     <label>
                         Nome da Categoria:
-                        <input 
+                        <input
                         type="text"
                         name="nome"
-                        value={values.nome} 
-                        
+                        value={values.nome}
+
                             //Essa função estava presente nas três entradas do formulário, contudo o formato dela permanece o mesmo somente mudando a entrada. Desse modo iremos criar uma função universal.
-                            //onChange= { function FuncaoHandLer(infosDoEvento){
-                            //console.log('[nomeDaCategoria]',nomeDaCategoria);
-                            //console.log('[InfosDoEvento.target.value]',infosDoEvento.target.value);--
+                            onChange= { function FuncaoHandLer(infosDoEvento){
+                            console.log('[nomeDaCategoria]',nomeDaCategoria);
+                            console.log('[InfosDoEvento.target.value]',infosDoEvento.target.value);--
                             //Não precisamos mais dos comentarios.
-                            //setNomeDaCategoria(infosDoEvento.target.value)}
-                            //Foi criado uma função suficientemente genérica que pode ser aplicado nos três.
+                            setNomeDaCategoria(infosDoEvento.target.value)}
+                            Foi criado uma função suficientemente genérica que pode ser aplicado nos três.
                             onChange={handleChange}
                         />
                     </label>
-                </div>*/}
-                {/*------DESCRIÇÂO--------- */}
-                <FormField
-                label="Descrição:"
-                type="????"
-                name="descricao"
-                value={values.descricao}
-                onChange={handleChange}
-                />
-                {/*------COR--------- */}
-                <FormField
-                label="Cor"
-                type="color"
-                name="cor"
-                value={values.cor}
-                onChange={handleChange}
-                />
-                <button>
-                    Cadastrar
-                </button>
-            </form>
+                </div> */}
+        {/* ------DESCRIÇÂO--------- */}
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+        {/* ------COR--------- */}
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}
+        />
+        <Button>
+          Cadastrar
+        </Button>
+      </form>
+      <div>
+        {/*Carregando... */}
+        Loading...
+      </div>
 
-            <ul>
-                {categorias.map((categoria, indice) => {
-                    {/*Estamos contatenando as duas informações*/}
-                    return(
-                        <li key={`${categoria}${indice}`}>
-                            {categoria}
-                        </li>
-                    )
-                })}
-            </ul>
-            
-            <Link to="/">
-                Ir para Home
-            </Link>
-        </PageDefault>
-    )
-    
+      <ul>
+        {categorias.map((categoria, indice) => {
+          { /* Estamos contatenando as duas informações */ }
+          return (
+            <li key={`${categoria}${indice}`}>
+              {categoria}
+            </li>
+          );
+        })}
+      </ul>
+
+      <Link to="/">
+        Ir para Home
+      </Link>
+    </PageDefault>
+  );
 }
 export default CadastroCategoria;
